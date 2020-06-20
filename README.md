e75e89702ae08a532d626aa71a8b7843be1707f4
Aslam Anver <slam.anver9@gmail.com>
2020-06-20 17:42:03 +0530

    Socket module

---
+++ README.md
@@ -0,0 +1,34 @@
+Socket session handler for Socket. IO
+
+[![NPM Version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][downloads-url]
+[![Linux Build][travis-image]][travis-url]
+
+``` js
+session_handler
+```
+
+## Installation
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/).
+
+Before installing, [download and install Node.js](https://nodejs.org/en/download/).
+Node.js 0.10 or higher is required.
+
+Installation is done using the
+
+``` bash
+$ npm install soss
+```
+
+## License
+
+  [MIT](LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/aslam.svg
+[npm-url]: https://npmjs.org/package/aslam
+[downloads-image]: https://img.shields.io/npm/dm/aslam.svg
+[downloads-url]: https://npmcharts.com/compare/aslam?minimal=true
+[travis-image]: https://img.shields.io/travis/aslamanver/aslam/master.svg?label=linux
+[travis-url]: https://travis-ci.org/aslamanver/aslam
---
+++ index.js
@@ -0,0 +1,104 @@
+const events = require('events');
+
+class IOSessionHandler {
+
+    #eventEmitter = new events.EventEmitter();
+
+    constructor() {
+        this.sessions = []
+    }
+
+    from(io, opts = { timeout: 5000 }) {
+
+        io.sockets.on('connection', (socket) => {
+
+            const token = socket.handshake.query.token
+            const id = socket.id
+
+            // Connected
+            this.onConnect(id, token, (data) => {
+                console.log(`${data.token} Connected`)
+                this.emitConnectionEvent(data, 1);
+            })
+
+            socket.on('disconnect', () => {
+                // Disconnected
+                this.onDisconnect(id, token, opts.timeout, (data) => {
+                    console.log(`${data.token} Disconnected`)
+                    this.emitConnectionEvent(data, 0);
+                })
+            })
+
+            socket.on('client_data', (client_data) => {
+                client_data = JSON.parse(client_data)
+                this.addData(client_data.token, client_data.data)
+            })
+        })
+
+        return this
+    }
+
+    connectionListener(listener) {
+        this.#eventEmitter.on('connectionListener', (connection) => listener(connection));
+    }
+
+    emitConnectionEvent(connection, status) {
+        connection.status = status;
+        this.#eventEmitter.emit('connectionListener', connection);
+    }
+
+    pushToken(id, token) {
+        if (this.getSession(token)) {
+            this.sessions.forEach((session) => {
+                if (session.token === token) {
+                    session.connections.push(id)
+                }
+            })
+        } else {
+            this.sessions.push({ token, connections: [id] })
+        }
+    }
+
+    getSession(token) {
+        return this.sessions.filter((session) => session.token === token)[0]
+    }
+
+    disconnectConnection(id, token) {
+        this.sessions.forEach((session, index) => {
+            if (session.token === token) {
+                session.connections.splice(session.connections.indexOf(id), 1)
+                if (session.connections.length == 0) {
+                    this.sessions.splice(index, 1)
+                }
+            }
+        })
+    }
+
+    onConnect(id, token, callback) {
+        if (!this.getSession(token)) callback({ id, token })
+        this.pushToken(id, token)
+    }
+
+    onDisconnect(id, token, timeout, callback) {
+        setTimeout(() => {
+            this.disconnectConnection(id, token)
+            if (!this.getSession(token)) callback({ id, token })
+        }, timeout)
+    }
+
+    onLogout() {
+        this.sessions.forEach((session) => {
+            if (session.token === token) {
+                session.connections.forEach((connection) => {
+                    io.to(connection).emit('logout')
+                })
+            }
+        })
+    }
+
+    addData(token, data) {
+        this.getSession(token).data = data
+    }
+}
+
+module.exports = new IOSessionHandler()
